telecomadm1145/CasioEmuMsvc

commit b32dcce1d8c325d1b8e87e225d8f3f6f859c302f

Context name should not be prepended for custom label name without leading dot

disassembler/disassembler.lua
@@ -141,10 +141,8 @@ do
 	local condition_names = {"ge", "lt", "gt", "le", "ges", "lts", "gts", "les", "ne", "eq", "nv", "ov", "ps", "ns", "al"}
 	formats = {
 		[ "lab"] = {format = function(self, value, instr)
-			if instr.context ~= value.context and not value.context_head then
-				if not value.context then
-					return value.name
-				end
+			if instr.context ~= value.context and not value.context_head and
+				value.context and value.name.sub(1, 1) == '.' then
 				return value.context.name .. value.name
 			end
 			return value.name

commit d69655587338171fcea12c1d6042cfa6feb6e8e2

Disassembler bugfix
The bug was caused by commit b32dcce1d8c325d1b8e87e225d8f3f6f859c302f: Context name should not be prepended for custom label name without leading dot.

disassembler/disassembler.lua
@@ -142,7 +142,7 @@ do
 	formats = {
 		[ "lab"] = {format = function(self, value, instr)
 			if instr.context ~= value.context and not value.context_head and
-				value.context and value.name.sub(1, 1) == '.' then
+				value.context and value.name:sub(1, 1) == '.' then
 				return value.context.name .. value.name
 			end
 			return value.name