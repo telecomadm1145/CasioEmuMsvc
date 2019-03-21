LBPHacker/CasioEmu

commit 5b29d4025a64aacd612e01e2e8437e2bd7003883

Only call Chipset::Frame when needed. Reduce CPU usage by up to 80% for main thread.

emulator/src/Peripheral/Screen.cpp
@@ -40,23 +40,27 @@ namespace casioemu
 
 		screen_buffer = new uint8_t[0x0200];
 
-		region_buffer.Setup(0xF800, 0x0200, "Screen/Buffer", screen_buffer, [](MMURegion *region, size_t offset) {
+		region_buffer.Setup(0xF800, 0x0200, "Screen/Buffer", this, [](MMURegion *region, size_t offset) {
 			offset -= region->base;
 			if ((offset & 0x000F) >= 0x000C)
 				return (uint8_t)0;
-			return ((uint8_t *)region->userdata)[offset];
+			return ((Screen *)region->userdata)->screen_buffer[offset];
 		}, [](MMURegion *region, size_t offset, uint8_t data) {
 			offset -= region->base;
 			if ((offset & 0x000F) >= 0x000C)
 				return;
-			((uint8_t *)region->userdata)[offset] = data;
+
+			auto this_obj = (Screen *)region->userdata;
+			// * Set require_frame to true only if the value changed.
+			this_obj->require_frame |= this_obj->screen_buffer[offset] != data;
+			this_obj->screen_buffer[offset] = data;
 		}, emulator);
 
-		region_range.Setup(0xF030, 1, "Screen/Range", &screen_range, MMURegion::DefaultRead<uint8_t, 0x07>, MMURegion::DefaultWrite<uint8_t, 0x07>, emulator);
+		region_range.Setup(0xF030, 1, "Screen/Range", this, MMURegion::DefaultRead<uint8_t, 0x07>, SetRequireFrameWrite<uint8_t, 0x07, &Screen::screen_range>, emulator);
 
-		region_mode.Setup(0xF031, 1, "Screen/Mode", &screen_mode, MMURegion::DefaultRead<uint8_t, 0x07>, MMURegion::DefaultWrite<uint8_t, 0x07>, emulator);
+		region_mode.Setup(0xF031, 1, "Screen/Mode", this, MMURegion::DefaultRead<uint8_t, 0x07>, SetRequireFrameWrite<uint8_t, 0x07, &Screen::screen_mode>, emulator);
 
-		region_contrast.Setup(0xF032, 1, "Screen/Contrast", &screen_contrast, MMURegion::DefaultRead<uint8_t, 0x1F>, MMURegion::DefaultWrite<uint8_t, 0x1F>, emulator);
+		region_contrast.Setup(0xF032, 1, "Screen/Contrast", this, MMURegion::DefaultRead<uint8_t, 0x1F>, SetRequireFrameWrite<uint8_t, 0x1F, &Screen::screen_contrast>, emulator);
 	}
 
 	void Screen::Uninitialise()
@@ -66,6 +70,8 @@ namespace casioemu
 
 	void Screen::Frame()
 	{
+		require_frame = false;
+
 		int ink_alpha_on = 20 + screen_contrast * 16;
 		if (ink_alpha_on > 255)
 			ink_alpha_on = 255;

emulator/src/Peripheral/Screen.hpp
@@ -48,6 +48,38 @@ namespace casioemu
 		static SpriteBitmap sprite_bitmap[];
 		SpriteInfo sprite_info[SPR_MAX];
 
+		/**
+		 * Similar to MMURegion::DefaultWrite, except this also set the
+		 * (require_frame) flag of (Peripheral) class.
+		 * If (only_on_change) is true, (require_frame) is not set if the new value
+		 * is the same as the old value.
+		 * (region->userdata) should be a pointer to a (Screen) instance.
+		 *
+		 * TODO: Probably this should be a member of Peripheral class instead.
+		 * (in that case (Screen) class needs to be parameterized)
+		 */
+		template<typename value_type, value_type mask = (value_type)-1,
+			value_type Screen:: *member_ptr, bool only_on_change = true>
+		static void SetRequireFrameWrite(MMURegion *region, size_t offset, uint8_t data)
+		{
+			auto this_obj = (Screen *)(region->userdata);
+			value_type &value = this_obj->*member_ptr;
+
+			value_type old_value;
+			if (only_on_change)
+				old_value = value;
+
+			// This part is identical to MMURegion::DefaultWrite.
+			// * TODO Try to avoid duplication?
+			value &= ~(((value_type)0xFF) << ((offset - region->base) * 8));
+			value |= ((value_type)data) << ((offset - region->base) * 8);
+			value &= mask;
+
+			if (only_on_change && old_value == value)
+				return;
+			this_obj->require_frame = true;
+		}
+
 	public:
 		using Peripheral::Peripheral;
 

emulator/src/Chipset/Chipset.cpp
@@ -14,6 +14,7 @@
 #include "../Peripheral/Timer.hpp"
 
 #include <fstream>
+#include <algorithm>
 
 namespace casioemu
 {
@@ -255,6 +256,13 @@ namespace casioemu
 	void Chipset::SetInterruptPendingSFR(size_t index)
 	{
 		data_int_pending |= (1 << (index - managed_interrupt_base));
+	}
+
+	bool Chipset::GetRequireFrame()
+	{
+		return std::any_of(peripherals.begin(), peripherals.end(), [](Peripheral *peripheral){
+			return peripheral->GetRequireFrame();
+		});
 	}
 
 	void Chipset::Frame()

emulator/src/Chipset/Chipset.hpp
@@ -90,6 +90,7 @@ namespace casioemu
 		bool GetInterruptPendingSFR(size_t index);
 
 		void Tick();
+		bool GetRequireFrame();
 		void Frame();
 		void UIEvent(SDL_Event &event);
 

emulator/src/Emulator.cpp
@@ -226,14 +226,16 @@ namespace casioemu
 	void Emulator::Frame()
 	{
 		std::lock_guard<std::recursive_mutex> access_lock(access_mx);
-
-		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-		SDL_RenderClear(renderer);
-		SDL_SetTextureColorMod(interface_texture, 255, 255, 255);
-		SDL_SetTextureAlphaMod(interface_texture, 255);
-		SDL_RenderCopy(renderer, interface_texture, &interface_background.src, &interface_background.dest);
-		chipset.Frame();
-		SDL_RenderPresent(renderer);
+		if (chipset.GetRequireFrame())
+		{
+			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+			SDL_RenderClear(renderer);
+			SDL_SetTextureColorMod(interface_texture, 255, 255, 255);
+			SDL_SetTextureAlphaMod(interface_texture, 255);
+			SDL_RenderCopy(renderer, interface_texture, &interface_background.src, &interface_background.dest);
+			chipset.Frame();
+			SDL_RenderPresent(renderer);
+		}
 	}
 
 	void Emulator::Tick()

emulator/src/Peripheral/Keyboard.cpp
@@ -123,6 +123,8 @@ namespace casioemu
 
 	void Keyboard::Frame()
 	{
+		require_frame = false;
+
 		SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
 		for (auto &button : buttons)
 		{
@@ -177,10 +179,13 @@ namespace casioemu
 				else
 					button.pressed = true;
 
+				require_frame = true;
+
 				if (button.type == Button::BT_POWER && button.pressed && !old_pressed)
 					emulator.chipset.Reset();
 				if (button.type == Button::BT_BUTTON && button.pressed != old_pressed)
 					RecalculateGhost();
+
 				break;
 			}
 		}
@@ -289,7 +294,10 @@ namespace casioemu
 			}
 		}
 		if (had_effect)
+		{
+			require_frame = true;
 			RecalculateGhost();
+		}
 	}
 
 	// * TODO: map computer keyboard keys to virtual buttons

emulator/src/Peripheral/Peripheral.cpp
@@ -2,7 +2,7 @@
 
 namespace casioemu
 {
-	Peripheral::Peripheral(Emulator &_emulator) : emulator(_emulator)
+	Peripheral::Peripheral(Emulator &_emulator) : emulator(_emulator), require_frame(false)
 	{
 	}
 
@@ -28,6 +28,7 @@ namespace casioemu
 
 	void Peripheral::Frame()
 	{
+		require_frame = false;
 	}
 
 	void Peripheral::UIEvent(SDL_Event &event)
@@ -37,5 +38,10 @@ namespace casioemu
 
 	void Peripheral::Reset()
 	{
+	}
+
+	bool Peripheral::GetRequireFrame()
+	{
+		return require_frame;
 	}
 }

emulator/src/Peripheral/Peripheral.hpp
@@ -12,6 +12,14 @@ namespace casioemu
 	protected:
 		Emulator &emulator;
 
+		/**
+		 * This should be true if the state of this peripheral changed
+		 * so that it requires a call to Frame().
+		 * It should not directly call Frame() because otherwise it may
+		 * call it more than required (once per timer_interval)
+		 */
+		bool require_frame;
+
 	public:
 		Peripheral(Emulator &emulator);
 		virtual void Initialise();
@@ -21,6 +29,7 @@ namespace casioemu
 		virtual void Frame();
 		virtual void UIEvent(SDL_Event &event);
 		virtual void Reset();
+		virtual bool GetRequireFrame();
 		virtual ~Peripheral();
 	};
 }

telecomadm1145/CasioEmuMsvc

commit 3acf0966f29e1982e9f014c74ca6496ef0a2e9a7

Fix wrong read result of screen SFRs
This has been broken since commit 5b29d4025a64aacd612e01e2e8437e2bd7003883.

emulator/src/Peripheral/Screen.cpp
@@ -59,11 +59,14 @@ namespace casioemu
 			this_obj->screen_buffer[offset] = data;
 		}, emulator);
 
-		region_range.Setup(0xF030, 1, "Screen/Range", this, MMURegion::DefaultRead<uint8_t, 0x07>, SetRequireFrameWrite<uint8_t, 0x07, &Screen::screen_range>, emulator);
+		region_range.Setup(0xF030, 1, "Screen/Range", this, DefaultRead<uint8_t, 0x07, &Screen::screen_range>,
+				SetRequireFrameWrite<uint8_t, 0x07, &Screen::screen_range>, emulator);
 
-		region_mode.Setup(0xF031, 1, "Screen/Mode", this, MMURegion::DefaultRead<uint8_t, 0x07>, SetRequireFrameWrite<uint8_t, 0x07, &Screen::screen_mode>, emulator);
+		region_mode.Setup(0xF031, 1, "Screen/Mode", this, DefaultRead<uint8_t, 0x07, &Screen::screen_mode>,
+				SetRequireFrameWrite<uint8_t, 0x07, &Screen::screen_mode>, emulator);
 
-		region_contrast.Setup(0xF032, 1, "Screen/Contrast", this, MMURegion::DefaultRead<uint8_t, 0x1F>, SetRequireFrameWrite<uint8_t, 0x1F, &Screen::screen_contrast>, emulator);
+		region_contrast.Setup(0xF032, 1, "Screen/Contrast", this, DefaultRead<uint8_t, 0x1F, &Screen::screen_contrast>,
+				SetRequireFrameWrite<uint8_t, 0x1F, &Screen::screen_contrast>, emulator);
 	}
 
 	void Screen::Uninitialise()

emulator/src/Peripheral/Screen.hpp
@@ -51,6 +51,19 @@ namespace casioemu
 		ColourInfo ink_colour;
 
 		/**
+		 * Similar to MMURegion::DefaultRead, but takes the pointer to the Screen
+		 * object as the userdata instead of the uint8_t member.
+		 */
+		template<typename value_type, value_type mask = (value_type)-1,
+			value_type Screen:: *member_ptr>
+		static uint8_t DefaultRead(MMURegion *region, size_t offset)
+		{
+			auto this_obj = (Screen *)(region->userdata);
+			value_type value = this_obj->*member_ptr;
+			return (value & mask) >> ((offset - region->base) * 8);
+		}
+
+		/**
 		 * Similar to MMURegion::DefaultWrite, except this also set the
 		 * (require_frame) flag of (Peripheral) class.
 		 * If (only_on_change) is true, (require_frame) is not set if the new value