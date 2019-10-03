LBPHacker/CasioEmu

commit 58ac2caa6bd8a7785924dff2b1272b45cded7571

More fixes and a timing adjustment

emulator/src/Emulator.cpp
@@ -10,7 +10,7 @@
 
 namespace casioemu
 {
-	Emulator::Emulator(std::map<std::string, std::string> &_argv_map, unsigned int _timer_interval, unsigned int _cycles_per_second, bool _paused) : paused(_paused), argv_map(_argv_map), cycles(_cycles_per_second), chipset(*new Chipset(*this))
+	Emulator::Emulator(std::map<std::string, std::string> &_argv_map, unsigned int _timer_interval, unsigned int _cycles_per_second, bool _paused) : paused(_paused), argv_map(_argv_map), cycles(_cycles_per_second, _timer_interval), chipset(*new Chipset(*this))
 	{
 		std::lock_guard<std::mutex> access_lock(access_mx);
 		running = true;
@@ -295,9 +295,11 @@ namespace casioemu
 		paused = _paused;
 	}
 
-	Emulator::Cycles::Cycles(Uint64 _cycles_per_second)
+	Emulator::Cycles::Cycles(Uint64 _cycles_per_second, unsigned int _timer_interval)
 	{
 		cycles_per_second = _cycles_per_second;
+		timer_interval = _timer_interval;
+		diff_cap = cycles_per_second * timer_interval / 1000;
 		performance_frequency = SDL_GetPerformanceFrequency();
 	}
 
@@ -312,6 +314,8 @@ namespace casioemu
 		Uint64 ticks_now = SDL_GetPerformanceCounter();
 		Uint64 cycles_to_have_been_emulated_by_now = (double)(ticks_now - ticks_at_reset) / performance_frequency * cycles_per_second;
 		Uint64 diff = cycles_to_have_been_emulated_by_now - cycles_emulated;
+		if (diff > diff_cap)
+			diff = diff_cap;
 		cycles_emulated = cycles_to_have_been_emulated_by_now;
 		return diff;
 	}

emulator/src/Emulator.hpp
@@ -59,10 +59,11 @@ namespace casioemu
 		 */
 		struct Cycles
 		{
-			Cycles(Uint64 cycles_per_second);
+			Cycles(Uint64 cycles_per_second, unsigned int timer_interval);
 			void Reset();
 			Uint64 GetDelta();
-			Uint64 ticks_at_reset, cycles_emulated, cycles_per_second, performance_frequency;
+			Uint64 ticks_at_reset, cycles_emulated, cycles_per_second, performance_frequency, diff_cap;
+			unsigned int timer_interval;
 		} cycles;
 
 	public:

emulator/src/Chipset/CPUArithmetic.cpp
@@ -10,6 +10,7 @@ namespace casioemu
 	void CPU::OP_ADD()
 	{
 		impl_flags_in &= ~PSW_C;
+		impl_flags_in |= PSW_Z;
 		OP_ADDC();
 	}
 
@@ -39,6 +40,8 @@ namespace casioemu
 	void CPU::OP_ADDC()
 	{
 		Add8();
+		if (!(impl_flags_in & PSW_Z))
+			impl_flags_out &= ~PSW_Z;
 		ZSCheck();
 	}
 
@@ -108,6 +111,7 @@ namespace casioemu
 	void CPU::OP_SUB()
 	{
 		impl_flags_in &= ~PSW_C;
+		impl_flags_in |= PSW_Z;
 		OP_SUBC();
 	}
 
@@ -116,6 +120,8 @@ namespace casioemu
 		impl_operands[0].value ^= 0xFF;
 		Add8();
 		impl_operands[0].value ^= 0xFF;
+		if (!(impl_flags_in & PSW_Z))
+			impl_flags_out &= ~PSW_Z;
 		ZSCheck();
 	}
 
@@ -160,19 +166,24 @@ namespace casioemu
 	void CPU::OP_DAA()
 	{
 		impl_operands[1].value = 0;
-		if ((impl_operands[0].value & 0x0F) > 0x09 || (impl_flags_in & PSW_HC)) impl_operands[1].value += 0x06;
-		if ((impl_operands[0].value & 0xF0) > 0x90 || (impl_flags_in &  PSW_C)) impl_operands[1].value += 0x60;
+		if ((impl_operands[0].value & 0x0F) > 0x09 || (impl_flags_in & PSW_HC)) impl_operands[1].value |= 0x06;
+		if ((impl_operands[0].value & 0xF0) > 0x90 || (impl_flags_in &  PSW_C)) impl_operands[1].value |= 0x60;
+		if ((impl_operands[0].value & 0xF0) == 0x90 && (impl_operands[0].value & 0x0F) > 0x09 && !(impl_flags_in & PSW_HC)) impl_operands[1].value |= 0x60;
+		uint8_t flags_in_backup = impl_flags_in;
 		OP_ADD();
-		impl_flags_out &= ~PSW_OV;
+		impl_flags_out |= flags_in_backup & PSW_C;
+		impl_flags_changed &= ~PSW_OV;
 	}
 
 	void CPU::OP_DAS()
 	{
 		impl_operands[1].value = 0;
-		if ((impl_operands[0].value & 0x0F) > 0x09 || (impl_flags_in & PSW_HC)) impl_operands[1].value += 0x06;
-		if ((impl_operands[0].value & 0xF0) > 0x90 || (impl_flags_in &  PSW_C)) impl_operands[1].value += 0x60;
+		if ((impl_operands[0].value & 0x0F) > 0x09 || (impl_flags_in & PSW_HC)) impl_operands[1].value |= 0x06;
+		if ((impl_operands[0].value & 0xF0) > 0x90 || (impl_flags_in &  PSW_C)) impl_operands[1].value |= 0x60;
+		uint8_t flags_in_backup = impl_flags_in;
 		OP_SUB();
-		impl_flags_out &= ~PSW_OV;
+		impl_flags_out |= flags_in_backup & PSW_C;
+		impl_flags_changed &= ~PSW_OV;
 	}
 
 	void CPU::OP_NEG()

emulator/src/Peripheral/Keyboard.cpp
@@ -291,5 +291,7 @@ namespace casioemu
 		if (had_effect)
 			RecalculateGhost();
 	}
+
+	// * TODO: map computer keyboard keys to virtual buttons
 }
 

emulator/src/Peripheral/ROMWindow.cpp
@@ -9,26 +9,40 @@ namespace casioemu
 {
 	void ROMWindow::Initialise()
 	{
-		region_0.Setup(0x00000, 0x08000, "ROM/Segment0", &emulator, [](MMURegion *region, size_t offset) {
-			return ((Emulator *)(region->userdata))->chipset.rom_data[offset];
+		region_0.Setup(0x00000, 0x08000, "ROM/Segment0", this, [](MMURegion *region, size_t offset) {
+			return ((ROMWindow *)(region->userdata))->emulator.chipset.rom_data[offset];
 		}, [](MMURegion *region, size_t offset, uint8_t data) {
-			logger::Info("ROM::[region write lambda]: attempt to write %02hhX to %06zX\n", data, offset);
-			((Emulator *)(region->userdata))->HandleMemoryError();
+			ROMWindow *rom_window = (ROMWindow *)(region->userdata);
+			if (rom_window->strict_memory)
+			{
+				logger::Info("ROM::[region write lambda]: attempt to write %02hhX to %06zX\n", data, offset);
+				rom_window->emulator.HandleMemoryError();
+			}
 		}, emulator);
 
-		region_1.Setup(0x10000, 0x10000, "ROM/Segment1", &emulator, [](MMURegion *region, size_t offset) {
-			return ((Emulator *)(region->userdata))->chipset.rom_data[offset];
+		region_1.Setup(0x10000, 0x10000, "ROM/Segment1", this, [](MMURegion *region, size_t offset) {
+			return ((ROMWindow *)(region->userdata))->emulator.chipset.rom_data[offset];
 		}, [](MMURegion *region, size_t offset, uint8_t data) {
-			logger::Info("ROM::[region write lambda]: attempt to write %02hhX to %06zX\n", data, offset);
-			((Emulator *)(region->userdata))->HandleMemoryError();
+			ROMWindow *rom_window = (ROMWindow *)(region->userdata);
+			if (rom_window->strict_memory)
+			{
+				logger::Info("ROM::[region write lambda]: attempt to write %02hhX to %06zX\n", data, offset);
+				rom_window->emulator.HandleMemoryError();
+			}
 		}, emulator);
 
-		region_8.Setup(0x80000, 0x10000, "ROM/Segment8", &emulator, [](MMURegion *region, size_t offset) {
-			return ((Emulator *)(region->userdata))->chipset.rom_data[offset - 0x80000];
+		region_8.Setup(0x80000, 0x10000, "ROM/Segment8", this, [](MMURegion *region, size_t offset) {
+			return ((ROMWindow *)(region->userdata))->emulator.chipset.rom_data[offset - 0x80000];
 		}, [](MMURegion *region, size_t offset, uint8_t data) {
-			logger::Info("ROM::[region write lambda]: attempt to write %02hhX to %06zX\n", data, offset);
-			((Emulator *)(region->userdata))->HandleMemoryError();
+			ROMWindow *rom_window = (ROMWindow *)(region->userdata);
+			if (rom_window->strict_memory)
+			{
+				logger::Info("ROM::[region write lambda]: attempt to write %02hhX to %06zX\n", data, offset);
+				rom_window->emulator.HandleMemoryError();
+			}
 		}, emulator);
+
+		strict_memory = emulator.argv_map.find("strict_memory") != emulator.argv_map.end();
 	}
 
 	void ROMWindow::Uninitialise()

emulator/src/Peripheral/ROMWindow.hpp
@@ -9,6 +9,7 @@ namespace casioemu
 	class ROMWindow : public Peripheral
 	{
 		MMURegion region_0, region_1, region_8;
+		bool strict_memory;
 
 	public:
 		using Peripheral::Peripheral;

telecomadm1145/CasioEmuMsvc

commit cc8a196eed318636db3ab2043879743120eb172c

Fix timer too slow
The problem was introduced in commit 58ac2caa6bd8a7785924dff2b1272b45cded7571.
The diff_cap being too small (it should be the ceil of the division, not
the floor) and the inaccurate timer makes the emulation slower than the
specified cycles_per_second.

emulator/src/Emulator.cpp
@@ -96,6 +96,7 @@ namespace casioemu
 		cycles.Reset();
 
 		tick_thread = new std::thread([this] {
+			auto iteration_end = std::chrono::steady_clock::now();
 			while (1)
 			{
 				{
@@ -105,7 +106,12 @@ namespace casioemu
 					TimerCallback();
 				}
 
-				std::this_thread::sleep_for(std::chrono::milliseconds(timer_interval));
+				iteration_end += std::chrono::milliseconds(timer_interval);
+				auto now = std::chrono::steady_clock::now();
+				if (iteration_end > now)
+					std::this_thread::sleep_until(iteration_end);
+				else // in case the computer is not fast enough or paused
+					iteration_end = now;
 			}
 		});
 
@@ -424,25 +430,23 @@ namespace casioemu
 
 	void Emulator::Cycles::Setup(Uint64 _cycles_per_second, unsigned int _timer_interval)
 	{
+		ticks_now = 0;
+		cycles_emulated = 0;
 		cycles_per_second = _cycles_per_second;
 		timer_interval = _timer_interval;
-		diff_cap = cycles_per_second * timer_interval / 1000;
-		performance_frequency = SDL_GetPerformanceFrequency();
 	}
 
 	void Emulator::Cycles::Reset()
 	{
-		ticks_at_reset = SDL_GetPerformanceCounter();
+		ticks_now = 0;
 		cycles_emulated = 0;
 	}
 
 	Uint64 Emulator::Cycles::GetDelta()
 	{
-		Uint64 ticks_now = SDL_GetPerformanceCounter();
-		Uint64 cycles_to_have_been_emulated_by_now = (double)(ticks_now - ticks_at_reset) / performance_frequency * cycles_per_second;
+		ticks_now += timer_interval;
+		Uint64 cycles_to_have_been_emulated_by_now = ticks_now * cycles_per_second / 1000;
 		Uint64 diff = cycles_to_have_been_emulated_by_now - cycles_emulated;
-		if (diff > diff_cap)
-			diff = diff_cap;
 		cycles_emulated = cycles_to_have_been_emulated_by_now;
 		return diff;
 	}

emulator/src/Emulator.hpp
@@ -61,13 +61,17 @@ namespace casioemu
 		 * it's queried for the number of cycles that need to be emulated in the
 		 * callback. This ensures that only as many cycles are emulated in a period
 		 * of time as many would be in real life.
+		 *
+		 * Note that it's assumed that the GetDelta function is called once every
+		 * timer_interval milliseconds. It's up to the timer to make sure that
+		 * there's no drift.
 		 */
 		struct Cycles
 		{
 			void Setup(Uint64 cycles_per_second, unsigned int timer_interval);
 			void Reset();
 			Uint64 GetDelta();
-			Uint64 ticks_at_reset, cycles_emulated, cycles_per_second, performance_frequency, diff_cap;
+			Uint64 ticks_now, cycles_emulated, cycles_per_second;
 			unsigned int timer_interval;
 		} cycles;
 