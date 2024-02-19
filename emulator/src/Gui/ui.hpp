ab1d2c0fedb0c58e7f53b801dd1e8bc68aaef225
Xyzst474 <104719291+Xyzst474@users.noreply.github.com>
2024-02-19 18:19:25 +0800

    Delete emulator directory

--- emulator/src/Gui/ui.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../Emulator.hpp"
-#include "CodeViewer.hpp"
-int test_gui();
-void gui_cleanup();
-void gui_loop();
-extern char *n_ram_buffer;
-extern casioemu::Emulator *m_emu;
-extern CodeViewer *code_viewer;
