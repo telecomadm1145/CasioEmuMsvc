c766d36b65c4b58b8d444f7c7e26547e31c76a68
telecomadm1145 <47657299+telecomadm1145@users.noreply.github.com>
2025-01-19 13:46:49 +0800

    Update SysDialog.h

--- CasioEmuMsvc/Ext/SysDialog.h
+++ CasioEmuMsvc/Ext/SysDialog.h
@@ -3,8 +3,14 @@
 #include <functional>
 class SystemDialogs {
 public:
+/*
 	static void OpenFileDialog(std::function<void(std::filesystem::path)>);
 	static void SaveFileDialog(std::string prefered_name, std::function<void(std::filesystem::path)>);
 	static void OpenFolderDialog(std::function<void(std::filesystem::path)>);
 	static void SaveFolderDialog(std::function<void(std::filesystem::path)>);
+*/
+	static std::filesystem::path OpenFileDialog();
+	static std::filesystem::path SaveFileDialog(std::string prefered_name);
+	static std::filesystem::path OpenFolderDialog();
+	static std::filesystem::path SaveFolderDialog();
 };
