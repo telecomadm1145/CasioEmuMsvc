d84ab439cad9f65040cce92a3a1113508c72ffa2
hieuxyz00 <khongbt446@gmail.com>
2026-01-25 10:29:10 +0700

    refactor: moved the immersive mode logic from SDLActivity to Game class and minor modifications

--- app/src/main/java/com/tele/u8emulator/Game.java
+++ app/src/main/java/com/tele/u8emulator/Game.java
@@ -4,6 +4,9 @@ import org.libsdl.app.SDLActivity;
 import android.os.Vibrator;
 import android.os.Environment;
 import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 import android.app.Activity;
 import android.net.Uri;
 import android.database.Cursor;
@@ -34,6 +37,41 @@ public class Game extends SDLActivity {
     private static Uri pendingUri = null;
     private static byte[] pendingData = null;
     private static int pendingRequestCode = -1;
+
+    private void setImmersiveMode() {
+        if (Build.VERSION.SDK_INT >= 19) {
+            View decorView = getWindow().getDecorView();
+            int flags = View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+
+            decorView.setSystemUiVisibility(flags);
+        }
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        if (Build.VERSION.SDK_INT >= 28) {
+            WindowManager.LayoutParams attributes = getWindow().getAttributes();
+            attributes.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER;
+            getWindow().setAttributes(attributes);
+        }
+        SDLActivity.onNativeResize();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setImmersiveMode();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            setImmersiveMode();
+        }
+    }
 
     public void onNativeCrash(String message) {
         Log.e(TAG, "Native crash: " + message);
--- app/src/main/java/org/libsdl/app/SDLActivity.java
+++ app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -222,24 +222,6 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
     // This is what SDL runs in. It invokes SDL_main(), eventually
     protected static Thread mSDLThread;
 
-    private void setImmersiveMode() {
-    if (Build.VERSION.SDK_INT >= 19) {
-        View decorView = getWindow().getDecorView();
-        int flags =
-            View.SYSTEM_UI_FLAG_FULLSCREEN |
-            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-
-        decorView.setSystemUiVisibility(flags);
-        }
-    SDLActivity.onNativeResize();
-    }
-
-
-
     protected static SDLGenericMotionListener_API12 getMotionListener() {
         if (mMotionListener == null) {
             if (Build.VERSION.SDK_INT >= 26 /* Android 8.0 (O) */) {
@@ -431,7 +413,6 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
         }
 
         setContentView(mLayout);
-        setImmersiveMode();
 
         setWindowStyle(false);
 
@@ -549,9 +530,6 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         Log.v(TAG, "onWindowFocusChanged(): " + hasFocus);
-        if (hasFocus) {
-            setImmersiveMode();
-        }
 
         if (SDLActivity.mBrokenLibraries) {
            return;
